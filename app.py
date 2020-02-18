4647ef4ac40552a3eb5323d4ec6767af010e157e
mattyturn95 <46969824+mattyturn95@users.noreply.github.com>
2020-02-18 23:56:16 +0000

    configured MONGO.DB in app.py

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import env
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'kerry_rivers'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', "Env Not loaded")
+mongo = PyMongo(app)
