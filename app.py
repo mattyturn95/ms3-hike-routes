e1342855fb2b9d848af899ff24410d77438a3011
mattyturn95 <46969824+mattyturn95@users.noreply.github.com>
2020-02-18 23:58:55 +0000

    home page function

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'hike_routes'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', "Env Not loaded")
 mongo = PyMongo(app)
+
+# Function to load HOME page:
+
+@app.route("/")
+@app.route("/get_hike_names")
+def get_hike_names():
+    return render_template("home.html", hikes=mongo.db.hike_names.find())
